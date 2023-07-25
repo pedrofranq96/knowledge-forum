f4675bfaaa6b12156a36da2696455f79efb238d3
pedrofranq96 <pedrofranq96@gmail.com>
2023-07-25 02:59:24 -0300

    backend - salvar usuário (estrutura)

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ const consign = require('consign')
 
 consign()
     .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
     .into(app)
+
 app.listen(3000, ()=> {
     console.log('backend em execução...')
 })
