7d2af69f3fd4f8280330ee401cb5a0fd5cdb43e1
pedrofranq96 <pedrofranq96@gmail.com>
2023-08-02 04:34:02 -0300

    frontend - componente Título da página/navegação

---
+++ frontend/src/components/admin/AdminPages.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="admin-pages">
+        <PageTitle icon="fa fa-cogs" main="Administração do Sistema"
+            sub="Controle de usuário"/>
+    </div>
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle'
+
+export default {
+    name: 'AdminPages',
+    components: { PageTitle }
+}
+</script>
+
+<style>
+
+</style>
