82d2fddc6c2b13f274f59c1d696809b68bdfe791
pedrofranq96 <pedrofranq96@gmail.com>
2023-08-04 01:44:37 -0300

    frontend - menu da aplicação

--- frontend/src/components/template/Menu.vue
+++ frontend/src/components/template/Menu.vue
@@ -1,15 +1,48 @@
 <template>
     <aside class="menu" v-show="isMenuVisible">
-        
+        <div class="menu-filter">
+            <i class="fa fa-search fa-lg"></i>
+            <input type="text" placeholder=" Digite para filtrar." v-model="treeFilter" class="filter-field">
+        </div>
+        <Tree :data="treeData" :options="treeOptions"  :filter="treeFilter" ref="tree"/>
     </aside>
 </template>
 
 <script>
 import {mapState} from 'vuex'
+import Tree from 'liquor-tree'
+import { baseApiUrl } from '@/global'
+import axios from 'axios'
 
 export default{
     name:"Menu",
-    computed: mapState(['isMenuVisible'])
+    components: { Tree },
+    computed: mapState(['isMenuVisible']),
+    data: function(){
+        return {
+            treeFilter: '',
+            treeData: this.getTreeData(),
+            treeOptions: { 
+                propertyNames: { 'text': 'name' },
+                filter: { emptyText: 'Categoria não encontrada.' }
+            }
+        }
+    },
+    methods: { 
+        getTreeData(){
+            const url = `${baseApiUrl}/categories/tree`
+            return axios.get(url).then(res => res.data)
+        },
+        onNodeSelect(node){
+            this.$router.push({
+                name: 'articlesByCategory',
+                params: { id: node.id }
+            })
+        }
+    },
+    mounted(){
+        this.$refs.tree.$on('node:selected', this.onNodeSelect)
+    }
 }
 </script>
 
@@ -21,5 +54,43 @@ export default{
         flex-direction: column;
         flex-wrap: wrap;
     }
+    .menu a,
+    .menu a:hover {
+        color: #fff;
+        text-decoration: none;
+    }
+    .menu .tree-node.selected .tree-content,
+    .menu .tree-node .tree-content:hover {
+        background-color: rgba(255,255,255,0.1);
+    }
+    .tree-arrow.has-child {
+        filter: brightness(2);
+    }
+    .menu .menu-filter {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin:15px;
+        padding-bottom: 8px;
+        border-bottom: 1px solid #AAA;
+    }
+    .menu .menu-filter i {
+        color: #AAA;
+        margin-right: 5px;
+    }
 
+    .menu input {
+        color: #CCC;
+        font-size:1.2rem;
+        border: 0;
+        outline: 0;
+        width: 100%;
+        border-radius: 8px;
+        background: transparent;
+    }
+    .tree-filter-empty {
+        color: #CCC;
+        margin-left: 20px;
+        font-size: 1.2rem;
+    }
 </style>
